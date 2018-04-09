9a08b731c0a6cd8e5a090f2876d65680e0091aa5
degro <degrootthierry@gmail.com>
2018-04-09 16:21:23 +0200

    help

--- src/com/company/stack.java
+++ src/com/company/stack.java
@@ -6,10 +6,6 @@ class MyGenericsStack<T extends Object> {
     private T[] stackArr;
     private int top;
 
-    /**
-     * constructor to create stack with size
-     * @param size
-     */
     @SuppressWarnings("unchecked")
     public MyGenericsStack(int size) {
         this.stackSize = size;
@@ -17,12 +13,6 @@ class MyGenericsStack<T extends Object> {
         this.top = -1;
     }
 
-    /**
-     * This method adds new entry to the top
-     * of the stack
-     * @param entry
-     * @throws Exception
-     */
     public void push(T entry){
         if(this.isStackFull()){
             System.out.println(("Stack is full. Increasing the capacity."));
@@ -32,12 +22,6 @@ class MyGenericsStack<T extends Object> {
         this.stackArr[++top] = entry;
     }
 
-    /**
-     * This method removes an entry from the
-     * top of the stack.
-     * @return
-     * @throws Exception
-     */
     public T pop() throws Exception {
         if(this.isStackEmpty()){
             throw new Exception("Stack is empty. Can not remove element.");
@@ -47,11 +31,6 @@ class MyGenericsStack<T extends Object> {
         return entry;
     }
 
-    /**
-     * This method returns top of the stack
-     * without removing it.
-     * @return
-     */
     public T peek() {
         return stackArr[top];
     }
@@ -67,19 +46,11 @@ class MyGenericsStack<T extends Object> {
         this.stackSize = this.stackSize*2;
     }
 
-    /**
-     * This method returns true if the stack is
-     * empty
-     * @return
-     */
+
     public boolean isStackEmpty() {
         return (top == -1);
     }
 
-    /**
-     * This method returns true if the stack is full
-     * @return
-     */
     public boolean isStackFull() {
         return (top == stackSize - 1);
     }
