25b4fb4c26057f56c3b792363d8101a6e96c5ef6
Stuxles <stuxles@live.nl>
2018-04-09 15:54:34 +0200

    sort

--- src/com/company/Quicksort.java
+++ src/com/company/Quicksort.java
@@ -48,24 +48,4 @@ public class Quicksort <T extends Comparable<T>>
             System.out.print(la[i]+" < ");
         System.out.println(la[ln-1]);
     }
-//    public static void main(String args[]){
-//        Quicksort qs1= new Quicksort();
-//        Quicksort qs2= new Quicksort();
-//        Quicksort qs4= new Quicksort();
-//
-//        Double  [] a1 = {0.12, 0.92, 0.23, .89, 0.42, 0.73, 0.34, 0.64, 0.51, 0.42, 0.5};
-//        Integer [] a2 = {1, 9, 2, 8, 3, 7, 4, 6, 5};
-//        String  [] a3 = {"me", "she", "you", "he"};
-//        Character    [] a4 = {'a', 'p', 'p', 'l','e','m','a','c','h','i','n','t','o','s','h'};
-//        qs4.sort(a4); //On completition of this call, a4 is sorted, but the result is printed later.
-//        qs1.sort(a1);
-//        qs2.sort(a2);
-//        qs1.displayArray();
-//        qs2.displayArray();
-//        qs2.sort(a3);
-//        qs2.displayArray(); // How do you print the result of sorted array a2 now?
-//        qs4.displayArray();
-//        qs1.displayArray(a2); //this would work with any instance qs1, qs2, qs3, qs4 or a new one.
-//        new Quicksort().displayArray(a3) ; // an instance with no name is created to print a3
-//    }
 }
