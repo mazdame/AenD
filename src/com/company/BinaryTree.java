34a8327aa882908246e3495cf7bb3aafb3173b96
JiggleWigglesnicker <games.j@live.nl>
2018-04-09 12:43:27 +0200

    Linked&Tree

---
+++ src/com/company/BinaryTree.java
@@ -0,0 +1,53 @@
+public class BinaryTree {
+    private TreeNode root;
+
+    public BinaryTree() {
+    }
+
+    public BinaryTree(int[] a) {
+        this();
+        for (int i : a) {
+            add(i);
+        }
+    }
+
+    private static class TreeNode {
+        TreeNode left;
+        int item;
+        TreeNode right;
+
+        TreeNode(TreeNode left, int item, TreeNode right) {
+            this.left = left;
+            this.right = right;
+            this.item = item;
+        }
+    }
+
+    public void add(int item) {
+        if (root == null) {
+            root = new TreeNode(null, item, null);
+            return;
+        }
+
+        TreeNode node = root;
+        while (true) {
+            if (item < node.item) {
+                if (node.left == null) {
+                    node.left = new TreeNode(null, item, null);
+                    break;
+                }
+                node = node.left;
+            } else if (item > node.item) {
+                if (node.right == null) {
+                    node.right = new TreeNode(null, item, null);
+                    break;
+                }
+                node = node.right;
+            } else {
+                break;
+            }
+        }
+    }
+
+}
+
