cb119bcf8ae677b655a1427b3a448f2084431d60
Stuxles <stuxles@live.nl>
2018-04-09 14:28:53 +0200

    qck

---
+++ src/com/company/QuicksortTest.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuicksortTest {
+
+    @Test
+    void sort() {
+    }
+
+    @Test
+    void quickSort() {
+    }
+
+    @Test
+    void partition() {
+    }
+
+    @Test
+    void displayArray() {
+    }
+
+    @Test
+    void displayArray1() {
+    }
+}
