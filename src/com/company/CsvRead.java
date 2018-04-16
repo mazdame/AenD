17e79015c0981d32a0a814afe1b599e48a714b97
Stuxles <stuxles@live.nl>
2018-04-16 14:39:44 +0200

    csvReader

--- src/com/company/CsvRead.java
+++ src/com/company/CsvRead.java
@@ -12,7 +12,38 @@ public class CsvRead {
         String line = "";
         String cvsSplitBy = ",";
 
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+
+            while((line = br.readLine()) != null) {
+
+                String[] number = line.split(cvsSplitBy);
+
+                String next = rmFirLas(number[0]);
+                if(isInt(next)) {
+                    test.add(Integer.parseInt(next));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return test;
+    }
+
+
+    private static String rmFirLas(String s) {
+        s = s.substring(1);
+        s = s.substring(0, s.length()-1);
+        return s;
+    }
+
+    private static boolean isInt(String s) {
+        try {
+            int y = Integer.parseInt(s);
+        } catch(NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 
 }
