8da5ba8e1702c24f696979d2f81c1434eed4dfe7
degro <degrootthierry@gmail.com>
2018-04-16 15:09:52 +0200

    added unit test

--- src/com/company/StackTest.java
+++ src/com/company/StackTest.java
@@ -1,45 +1,33 @@
 package com.company;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(Arquillian.class)
-public class StackTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(MyGenericsStack.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+class stackTest {
+
+    @Test
+    void push() {
+
     }
 
     @Test
-    public void push() {
+    void pop() {
     }
 
     @Test
-    public void pop() {
+    void peek() {
     }
 
     @Test
-    public void peek() {
+    void isStackEmpty() {
     }
 
     @Test
-    public void isStackEmpty() {
+    void isStackFull() {
     }
 
     @Test
-    public void isStackFull() {
-    }
-
-    @Test
-    public void main() {
+    void main() {
     }
 }
