3de9f7b6df98e7873af2ac4f277ed78b065f0c09
JiggleWigglesnicker <games.j@live.nl>
2018-04-16 16:15:40 +0200

    fixed

--- src/com/company/Binarysearch.java
+++ src/com/company/Binarysearch.java
@@ -9,22 +9,22 @@ public class Binarysearch {
             if (cmp < 0) return binarySearch(array, value, lo, mid - 1);
             if (cmp > 0) return binarySearch(array, value, mid + 1, hi);
             return mid;
-        } // if
+        }
         return -1;
-    } // binarySearch
+    }
 
     public static <T extends Comparable<T>> int linearSearch(T[] array, T value, int lo, int hi) {
         for (int i = lo; i <= hi; i++) {
             if (array[i].compareTo(value) == 0) return i;
-        } // for
+        }
         return -1;
-    } // linearSearch
+    }
 
     public static <T> void swap(T[] array, int lhs, int rhs) {
         T tmp = array[lhs];
         array[lhs] = array[rhs];
         array[rhs] = tmp;
-    } // swap
+    }
 
     public static <T extends Comparable<T>> int partition(T[] array, int piv, int lo, int hi) {
         T val = array[piv];
@@ -32,7 +32,7 @@ public class Binarysearch {
             while (array[lo].compareTo(val) < 0) lo++;
             while (array[hi].compareTo(val) > 0) hi--;
             swap(array, lo, hi);
-        } // while
+        }
         return lo;
-    } // partition
-} // Search
+    }
+}
