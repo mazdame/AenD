4b092c45393c17ef5377a2fc74a8cb213d704e1f
degro <degrootthierry@gmail.com>
2018-04-16 11:53:43 +0200

    help

---
+++ src/com/company/StackTest.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class StackTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(MyGenericsStack.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    public void push() {
+    }
+
+    @Test
+    public void pop() {
+    }
+
+    @Test
+    public void peek() {
+    }
+
+    @Test
+    public void isStackEmpty() {
+    }
+
+    @Test
+    public void isStackFull() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
