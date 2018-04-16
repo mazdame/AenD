28e7bc954d6b766270d281463218de3ef3dd23b0
degro <degrootthierry@gmail.com>
2018-04-16 15:36:36 +0200

    added unit test

---
+++ src/com/company/LinkedListTest.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    @Test
+    void addFirst() {
+    }
+
+    @Test
+    void removeFirst() {
+    }
+
+    @Test
+    public String toString() {
+        return null;
+    }
+
+    @Test
+    void isEmpty() {
+    }
+}
