6a1fa72829fc134e5e0c7b5a9c3328a54272ef75
Stuxles <stuxles@live.nl>
2018-04-16 16:00:01 +0200

    quee back

--- src/com/company/queue.java
+++ src/com/company/queue.java
@@ -1,3 +1,37 @@
 package com.company;
 
 import java.util.*;
+
+class Queue<T> implements Iterable<T> {
+
+    private LinkedList<T> elements = new LinkedList<T>();
+
+    public void enqueue(T element) {
+        elements.add(element);
+    }
+
+    public T dequeue() {
+        return elements.removeFirst();
+    }
+
+    public T peek() {
+        return elements.getFirst();
+    }
+
+    public void clear() {
+        elements.clear();
+    }
+
+    public int size() {
+        return elements.size();
+    }
+
+    public boolean isEmpty() {
+        return elements.isEmpty();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return elements.iterator();
+    }
+}
